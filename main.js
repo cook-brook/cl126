3b4d9e250e3d992e01ac0e21dd9b7434303bf62b
cook-brook <72130874+cook-brook@users.noreply.github.com>
2021-08-22 15:31:03 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 song="";
+leftWristX=0;
+leftWristY=0;
+rightWristX=0;
+rightWristY=0;
 
 function preload() { 
     song = loadSound("music.mp3");
@@ -10,6 +14,25 @@ function setup(){
 
 	video = createCapture(VIDEO);
 	video.hide();
+
+    poseNet=ml5.poseNet(video,modelLoaded);
+    poseNet.on('results',gotPoses);
+}
+function modelLoaded(){
+    console.log('poseNet is initialized');
+}
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+        leftWristX=results[0].pose.leftWrist.x;
+        leftWristY=results[0].pose.leftWrist.y;
+        console.log("leftWristX="+leftWristX+"leftWristY"+leftWristY);
+
+        rightWristX=results[0].pose.rightWrist.x;
+        rightWristY=results[0].pose.rightWrist.y;
+        console.log("rightWristX="+rightWristX+"rightWristY="+ightWristY);
+    }
+
 }
 
 function draw(){
@@ -18,4 +41,6 @@ function draw(){
 
 function play(){
     song.play();
+    song.setVolume(1);
+    song.rate(1);
 }
